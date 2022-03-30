244364b70d85d1bfd2c0212baddd71fe07057068
Samuel Ceballos <sceballosp@eafit.edu.co>
2022-03-30 11:54:05 -0500

    Cambios al layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -7,13 +8,13 @@
   <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
   <title>@yield('title', 'Online Store')</title>
 </head>
+
 <body>
   <!-- header -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
     <div class="container">
-      <a class="navbar-brand" href="{{ route('home.index') }}">Online Store</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="{{ route('home.index') }}">Computer Store</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -29,8 +30,7 @@
           @else
           <a class="nav-link active" href="{{ route('order.list') }}">My Orders</a>
           <form id="logout" action="{{ route('logout') }}" method="POST">
-            <a role="button" class="nav-link active"
-               onclick="document.getElementById('logout').submit();">Logout</a>
+            <a role="button" class="nav-link active" onclick="document.getElementById('logout').submit();">Logout</a>
             @csrf
           </form>
           @endguest
@@ -54,8 +54,7 @@
   <div class="copyright py-4 text-center text-white">
     <div class="container">
       <small>
-        Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
-          href="https://twitter.com/danielgarax">
+        Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank" href="https://twitter.com/danielgarax">
           Daniel Correa
         </a> - <b>Paola Vallejo</b>
       </small>
@@ -65,4 +64,5 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
   </script>
 </body>
+
 </html>
