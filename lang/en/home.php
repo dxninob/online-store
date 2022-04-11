b5508adc3a146562e5bf80845b97d02c2ad5223b
Daniela Nino <danielanino194@gmail.com>
2022-04-11 18:18:31 -0500

    Se añadió about a lang

---
+++ lang/en/home.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the home application messages. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'index.title' => 'Home Page - Online Store',
+
+    'about.title' => 'About us - Online Store',
+    'about.subtitle' => 'About us',
+    'about.description' => 'Esta es una página sobre nosotros...',
+    'about.author' => 'Desarrollado por: Samuel Ceballos, Juan Pablo Madrid y Daniela Niño',
+];
