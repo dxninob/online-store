a3623b6d4267a5d405e216cb88199a31585e2196
Daniela Nino <danielanino194@gmail.com>
2022-04-11 19:14:03 -0500

    Se añadió layouts a lang

---
+++ lang/en/layouts.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Layouts Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the Layouts application messages. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'admin.title' => 'Admin - Online Store',
+    'admin.user' => 'Administrador',
+    'admin.panel' => 'Panel de administrador',
+    'admin.home' => '- Admin - Inicio',
+    'admin.computers' => '- Admin - Computadores',
+    'admin.categories' => '- Admin - Categorias',
+    'admin.statistics' => '- Admin - Estadisticas',
+    'admin.back' => 'Volver a la página de inicio',
+
+    'app.title' => 'Online Store',
+    'app.subtitle' => 'A Laravel Online Store',
+    'app.store' => 'Tienda de Computadores',
+    'app.home' => 'Inicio',
+    'app.computers' => 'Computadores',
+    'app.order' => 'Orden',
+    'app.about' => 'Sobre nosotros',
+    'app.login' => 'Iniciar sesión',
+    'app.register' => 'Registarse',
+    'app.logout' => 'Cerrar sesión',
+    'app.myOrder' => 'Mi orden',
+];
